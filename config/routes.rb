393f68f3ec9b9849c90ecfedd31959bbf724a9a6
Jason Hall <hall.jasondaniel@gmail.com>
2017-06-27 17:51:18 -0600

    created nav and index and show for blogs with basic styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'blogs#index'
 end
